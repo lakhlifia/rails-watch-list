c9846b51a95c60515708a6167740a141da5e38c6
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-02-25 19:18:27 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root 'lists#index'
+  root "lists#index"
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :destroy]
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
   end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
